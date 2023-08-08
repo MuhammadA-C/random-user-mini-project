c19f5ceb084b5f71c2e054e71a21acd202557a80
MuhammadA-C <phantomganhdi@gmail.com>
2023-08-08 01:13:07 -0700

    Fetch API
    Added the code to do a fetch api call to the random user api, then updated the DOM elements with the info.

--- javascript/script.js
+++ javascript/script.js
@@ -20,6 +20,28 @@ const ageDOM = document.querySelector(
   ".personal-info-container > :nth-child(5)"
 );
 
-console.log(imgDOM);
-
 ///////////////////////////////////////////////////////////////////
+
+btnDOM.addEventListener("click", () => {
+  fetch("https://randomuser.me/api/")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      let name = `${data.results[0].name.first} ${data.results[0].name.last}`;
+      let email = data.results[0].email;
+      let age = data.results[0].dob.age;
+      let location = data.results[0].location.country;
+      let phone = data.results[0].phone;
+      let picture = data.results[0].picture.large;
+
+      imgDOM.src = `${picture}`;
+      nameDOM.textContent = `Name: ${name}`;
+      emailDOM.textContent = `Email: ${email}`;
+      phoneDOM.textContent = `Phone: ${phone}`;
+      locationDOM.textContent = `Location: ${location}`;
+      ageDOM.textContent = `Age: ${age}`;
+
+      return data;
+    });
+});
