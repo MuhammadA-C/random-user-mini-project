b0de5206c7b775bc6326ec8a8777748dfe959844
MuhammadA-C <phantomganhdi@gmail.com>
2023-08-08 00:49:37 -0700

    Basic GUI Setup
    Created the basic GUI for this mini project.

---
+++ javascript/script.js
@@ -0,0 +1,25 @@
+///////////////////////////////////////////////////////////////////
+
+const btnDOM = document.querySelector("button");
+const imgDOM = document.querySelector("img");
+const nameDOM = document.querySelector(
+  ".personal-info-container > :nth-child(1)"
+);
+const emailDOM = document.querySelector(
+  ".personal-info-container > :nth-child(2)"
+);
+const phoneDOM = document.querySelector(
+  ".personal-info-container > :nth-child(3)"
+);
+
+const locationDOM = document.querySelector(
+  ".personal-info-container > :nth-child(4)"
+);
+
+const ageDOM = document.querySelector(
+  ".personal-info-container > :nth-child(5)"
+);
+
+console.log(imgDOM);
+
+///////////////////////////////////////////////////////////////////
